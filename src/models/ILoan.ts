5b04f5894e34a0b9487a77c9511442e0cac7cd8b
sammysium <46919720+sammysium@users.noreply.github.com>
2024-08-11 22:57:26 +0300

    setup data models

---
+++ src/models/ILoan.ts
@@ -0,0 +1,8 @@
+interface ILoan {
+    id: number;
+    name: string;
+    interestRate: number;
+    maximumAmount: number;
+}
+
+export default ILoan;
---
+++ src/models/ILoanApplication.ts
@@ -0,0 +1,10 @@
+
+interface ILoanApplication {
+    full_name: string;
+    email: string;
+    loan_amount: number;
+    loan_purpose: string;
+}
+
+
+export default ILoanApplication;
